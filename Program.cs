52833c3be60f0b6a67f08aaca157177c4907fdd8
fre-beep <satfire500@gmail.com>
2025-06-02 08:16:46 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        // Initialize database
+        DatabaseHelper.InitializeDatabase();
+
+        // Start with login form
+        Application.Run(new LoginForm());
+    }
+}
